0db9a0b6d963a88b4cf056f24bb8af68da51cfeb
jefferson venceslau <jefferson.venceslau@pgmais.com.br>
2022-03-31 20:16:18 -0300

    feat: doc and all

--- benchmarks/index.js
+++ benchmarks/index.js
@@ -1,80 +1,123 @@
 'use strict';
 
-const Benchmark = require('benchmark');
+const { Suite } = require('benchmark');
+const suite = new Suite();
 
-const { v4 } = require('uuid');
-const { randomFillSync } = require('crypto');
-const { uuidV4 } = require('fast-uuidv4');
-class UUID {
-  static #byteToHex = Array.from({ length: 256 }, (_, i) => (i + 0x100).toString(16).substr(1));
-  static #random8Pool = new Uint8Array(256);
-  static #poolLength = UUID.#random8Pool.length;
-  static #generateRandom() {
-    if (UUID.#poolLength > UUID.#random8Pool.length - 16) {
-      randomFillSync(UUID.#random8Pool);
-      UUID.#poolLength = 0;
-    }
-    return UUID.#random8Pool.slice(UUID.#poolLength, UUID.#poolLength += 16);
-  }
+{
+  try {
+    const name = 'lib uuid js';
+    const { v4 } = require('uuid');
 
-  static #stringify(arr, offset = 0) {
-    return (
-      UUID.#byteToHex[arr[offset + 0]] +
-      UUID.#byteToHex[arr[offset + 1]] +
-      UUID.#byteToHex[arr[offset + 2]] +
-      UUID.#byteToHex[arr[offset + 3]] + '-' +
-      UUID.#byteToHex[arr[offset + 4]] +
-      UUID.#byteToHex[arr[offset + 5]] + '-' +
-      UUID.#byteToHex[arr[offset + 6]] +
-      UUID.#byteToHex[arr[offset + 7]] + '-' +
-      UUID.#byteToHex[arr[offset + 8]] +
-      UUID.#byteToHex[arr[offset + 9]] + '-' +
-      UUID.#byteToHex[arr[offset + 10]] +
-      UUID.#byteToHex[arr[offset + 11]] +
-      UUID.#byteToHex[arr[offset + 12]] +
-      UUID.#byteToHex[arr[offset + 13]] +
-      UUID.#byteToHex[arr[offset + 14]] +
-      UUID.#byteToHex[arr[offset + 15]]
-    );
-  }
-  static v4() {
-    const random = UUID.#generateRandom();
-    random[6] = random[6] & 0x0f | 0x40;
-    random[8] = random[8] & 0x3f | 0x80;
-    return UUID.#stringify(random);
+    suite.
+      add(name, {
+        defer: true,
+        fn: (deferred) => {
+          v4();
+          deferred.resolve();
+        }
+      });
+
+  } catch (e) {
+    console.error(e);
   }
 }
 
+{
+  try {
+    const { randomFillSync } = require('crypto');
 
-const suite = new Benchmark.Suite;
+    const name = 'js using crypto randomFillSync';
+    class UUID {
+      static #byteToHex = Array.from({ length: 256 }, (_, i) => (i + 0x100).toString(16).substring(1));
+      static #random8Pool = new Uint8Array(256);
+      static #poolLength = UUID.#random8Pool.length;
+      static #generateRandom() {
+        if (UUID.#poolLength > UUID.#random8Pool.length - 16) {
+          randomFillSync(UUID.#random8Pool);
+          UUID.#poolLength = 0;
+        }
+        return UUID.#random8Pool.slice(UUID.#poolLength, UUID.#poolLength += 16);
+      }
 
-suite.
-  add('uuid', {
-    defer: true,
-    fn: function (deferred) {
-
-      v4();
-      deferred.resolve();
+      static #stringify(arr, offset = 0) {
+        return (
+          UUID.#byteToHex[arr[offset + 0]] +
+          UUID.#byteToHex[arr[offset + 1]] +
+          UUID.#byteToHex[arr[offset + 2]] +
+          UUID.#byteToHex[arr[offset + 3]] + '-' +
+          UUID.#byteToHex[arr[offset + 4]] +
+          UUID.#byteToHex[arr[offset + 5]] + '-' +
+          UUID.#byteToHex[arr[offset + 6]] +
+          UUID.#byteToHex[arr[offset + 7]] + '-' +
+          UUID.#byteToHex[arr[offset + 8]] +
+          UUID.#byteToHex[arr[offset + 9]] + '-' +
+          UUID.#byteToHex[arr[offset + 10]] +
+          UUID.#byteToHex[arr[offset + 11]] +
+          UUID.#byteToHex[arr[offset + 12]] +
+          UUID.#byteToHex[arr[offset + 13]] +
+          UUID.#byteToHex[arr[offset + 14]] +
+          UUID.#byteToHex[arr[offset + 15]]
+        );
+      }
+      static v4() {
+        const random = UUID.#generateRandom();
+        random[6] = random[6] & 0x0f | 0x40;
+        random[8] = random[8] & 0x3f | 0x80;
+        return UUID.#stringify(random);
+      }
     }
-  });
-  
-suite.
-  add('crypto nodejs', {
-    defer: true,
-    fn: function (deferred) {
-      UUID.v4();
-      deferred.resolve();
-    }
-  });
 
-suite.
-  add('fast-uuidv4', {
-    defer: true,
-    fn: function (deferred) {
-      uuidV4();
-      deferred.resolve();
-    }
-  });
+    suite.
+      add(name, {
+        defer: true,
+        fn: (deferred) => {
+          UUID.v4();
+          deferred.resolve();
+        }
+      });
+
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+{
+  try {
+    const { randomUUID } = require('crypto');
+
+    const name = 'js using crypto randomUUID';
+
+    suite.
+      add(name, {
+        defer: true,
+        fn: (deferred) => {
+          randomUUID();
+          deferred.resolve();
+        }
+      });
+
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+{
+  try {
+    const { uuidV4 } = require('fast-uuidv4');
+    const name = 'this lib fast-uuidv4';
+
+    suite.
+      add(name, {
+        defer: true,
+        fn: (deferred) => {
+          uuidV4();
+          deferred.resolve();
+        }
+      });
+  } catch (e) {
+    console.error(e);
+  }
+}
 
 suite.
   on("cycle", function (event) {
@@ -87,5 +130,5 @@ suite.
   });
 
 suite.
-  run();
+  run({ async: true });
 
